57f6831e0f90b48a25da1149c509c844407e5efc
valterteodorobsi <valterteodorobsi@gmail.com>
2015-03-13 14:29:22 +0000

    dao e model de relatorio

--- TccSisEx/src/controller/RelatorioGerencialController.java
+++ TccSisEx/src/controller/RelatorioGerencialController.java
@@ -1,15 +1,28 @@
 package controller;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
-import model.Medico;
-import dao.MedicoDaoImp;
+import model.RelatorioGerencial;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import net.sf.jasperreports.view.JasperViewer;
 
-@ManagedBean(name = "gerencialBen")
+import org.primefaces.model.chart.PieChartModel;
+
+import util.TipoEntrada;
+import dao.RelatorioGerencialDao;
+
+@ManagedBean(name = "gerencialBean")
 @ViewScoped
 public class RelatorioGerencialController implements Serializable {
 
@@ -17,41 +30,132 @@ public class RelatorioGerencialController implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	private Medico medico;
-	private List<Medico> listMedico;
+
+	private RelatorioGerencial gerencial;
+	private List<RelatorioGerencial> listaGerencial = new ArrayList<RelatorioGerencial>();
+	private Date dataInicial;
+	private Date dataFinal;
+	private String path; // Caminho base
+	private String pathToReportPackage; // Caminho para o package onde estão armazenados os relatorios Jarper
+	private PieChartModel piechart;								 
 
 	public RelatorioGerencialController() {
+		gerencial = new RelatorioGerencial();
+		piechart = new PieChartModel();
+		this.path = this.getClass().getClassLoader().getResource("").getPath();
+		this.pathToReportPackage = this.path + "jasper/";
+		System.out.println(path);
+	}
+
+	public void imprimir() throws Exception {
+		RelatorioGerencialDao dao = new RelatorioGerencialDao();
+        String nomeMedico = gerencial.getNomeMedico();
+        
+        if(gerencial.getNomeMedico().equals("todos")){
+        	
+        	listaGerencial =  dao.relatorioGerencialTodos(nomeMedico);
+        }
+        else{
+              
+        listaGerencial =  dao.relatorioGerencial(nomeMedico);
+		}
+        
+        List<RelatorioGerencial> relatorioGerencial = new ArrayList<RelatorioGerencial>();
+        
+        for(RelatorioGerencial gerencial : listaGerencial){
+        	
+        	relatorioGerencial.add(gerencial);
+        	        	           
+    }
+        
+       
+		JasperReport report = JasperCompileManager.compileReport(this
+				.getPathToReportPackage() + "Relatorio_Gerencial.jrxml");
+
+		JasperPrint print = JasperFillManager.fillReport(report, null,
+				new JRBeanCollectionDataSource(relatorioGerencial));
+		// abre visualizador
+		JasperViewer jv = new JasperViewer(print, false);
+		jv.setTitle("Relatorio Gerencial");
+		jv.setVisible(true);
+
+		JasperExportManager.exportReportToPdfFile(print,
+				"c:/relatorio/Relatorio_Gerencial.pdf");
+	}
+	
+	public void abreGrafico(){
 		
-		listMedico = new MedicoDaoImp().list();
-	} 
-		
+		if(gerencial != null){
+                
+        piechart = new PieChartModel();
+        RelatorioGerencialDao dao = new RelatorioGerencialDao();
+        String nomeMedico = gerencial.getNomeMedico();
+       
+        listaGerencial =  dao.relatorioGerencial(nomeMedico);
+                
+                piechart = new PieChartModel();
+                
+                for(RelatorioGerencial gerencial : listaGerencial){
+                        piechart.set(TipoEntrada.getByOrdinal( Integer.valueOf(gerencial.getTipoEntrada())).getDescricao(), gerencial.getQtdeEntrada());
+                }
+        }
+        
+}
 	
-	
-	
-	
-	
-	public Medico getMedico() {
-		return medico;
+	public PieChartModel getPiechart() {
+		return piechart;
 	}
 
-
-	public void setMedico(Medico medico) {
-		this.medico = medico;
+	public void setPiechart(PieChartModel piechart) {
+		this.piechart = piechart;
 	}
 
-
-	public List<Medico> getListMedico() {
-		return listMedico;
+	public String getPath() {
+		return path;
 	}
 
-
-	public void setListMedico(List<Medico> listMedico) {
-		this.listMedico = listMedico;
+	public void setPath(String path) {
+		this.path = path;
 	}
-	
-	
 
+	public String getPathToReportPackage() {
+		return pathToReportPackage;
+	}
 
+	public void setPathToReportPackage(String pathToReportPackage) {
+		this.pathToReportPackage = pathToReportPackage;
+	}
+
+	public RelatorioGerencial getGerencial() {
+		return gerencial;
+	}
+
+	public void setGerencial(RelatorioGerencial gerencial) {
+		this.gerencial = gerencial;
+	}
+
+	public List<RelatorioGerencial> getListaGerencial() {
+		return listaGerencial;
+	}
+
+	public void setListaGerencial(List<RelatorioGerencial> listaGerencial) {
+		this.listaGerencial = listaGerencial;
+	}
+
+	public Date getDataInicial() {
+		return dataInicial;
+	}
+
+	public void setDataInicial(Date dataInicial) {
+		this.dataInicial = dataInicial;
+	}
+
+	public Date getDataFinal() {
+		return dataFinal;
+	}
+
+	public void setDataFinal(Date dataFinal) {
+		this.dataFinal = dataFinal;
+	}
 
 }
