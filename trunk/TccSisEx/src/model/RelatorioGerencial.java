bc5b6cdba13708b37a05fed74d67992c7e90be8a
valterteodorobsi@gmail.com <valterteodorobsi@gmail.com@46f70359-e648-01da-4420-93a02d495d7f>
2015-03-13 14:29:22 +0000

    dao e model de relatorio

--- trunk/TccSisEx/src/model/RelatorioGerencial.java
+++ trunk/TccSisEx/src/model/RelatorioGerencial.java
@@ -1,7 +1,48 @@
 package model;
 
-public class RelatorioGerencial {
+import java.io.Serializable;
+
+public class RelatorioGerencial implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private String nomeMedico;
+	private String tipoEntrada;
+	private float qtdeEntrada;
 	
 	
+	
+	public RelatorioGerencial() {
+		
+	}
+	
+	public RelatorioGerencial(String nomeMedico, String tipoEntrada,float qtdeEntrada) {
+		this.nomeMedico = nomeMedico;
+		this.tipoEntrada = tipoEntrada;
+		this.qtdeEntrada = qtdeEntrada;
+	}
+	public String getNomeMedico() {
+		return nomeMedico;
+	}
+	public void setNomeMedico(String nomeMedico) {
+		this.nomeMedico = nomeMedico;
+	}
+	public String getTipoEntrada() {
+		return tipoEntrada;
+	}
+	public void setTipoEntrada(String tipoEntrada) {
+		this.tipoEntrada = tipoEntrada;
+	}
+	public float getQtdeEntrada() {
+		return qtdeEntrada;
+	}
+	public void setQtdeEntrada(float qtdeEntrada) {
+		this.qtdeEntrada = qtdeEntrada;
+	}
+	
+
 	
 }
