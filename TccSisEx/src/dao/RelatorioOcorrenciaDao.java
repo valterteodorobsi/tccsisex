fff6c8c50dcb55b9cfe874611596d5e0f92078a0
valterteodorobsi <valterteodorobsi@gmail.com>
2015-06-18 14:33:15 +0000

    alteração na query  group by

--- TccSisEx/src/dao/RelatorioOcorrenciaDao.java
+++ TccSisEx/src/dao/RelatorioOcorrenciaDao.java
@@ -17,7 +17,7 @@ public class RelatorioOcorrenciaDao {
 		Transaction t = session.beginTransaction();
 		
 		
-		SQLQuery query = session.createSQLQuery("select f.nome as nomeColaborador , enc_ex as encaminhamento ,  seto.nome as nomeSetor, COUNT(enc_ex) as qtdEncaminhamento  from registro_entrada as r inner join funcionario AS f on f.ID_MATRICULA = r.ID_MATRICULA inner join setor AS seto on f.ID_CENTRO_CUSTO = seto.ID_CENTRO_CUSTO where seto.ID_CENTRO_CUSTO = :nomeSetor or f.ID_MATRICULA = :nomeColaborador group by f.nome  , enc_ex ,  seto.nome");
+		SQLQuery query = session.createSQLQuery("select f.nome as nomeColaborador , enc_ex as encaminhamento ,  seto.nome as nomeSetor, COUNT(enc_ex) as qtdEncaminhamento  from registro_entrada as r inner join funcionario AS f on f.ID_MATRICULA = r.ID_MATRICULA inner join setor AS seto on f.ID_CENTRO_CUSTO = seto.ID_CENTRO_CUSTO where seto.ID_CENTRO_CUSTO = :nomeSetor or f.ID_MATRICULA = :nomeColaborador group by enc_ex, f.nome ,  seto.nome");
 		query.setParameter("nomeColaborador", nomeColaborador);
 		query.setParameter("nomeSetor", nomeSetor);
 	    query.addScalar("nomeColaborador");
@@ -40,7 +40,7 @@ public class RelatorioOcorrenciaDao {
 		Transaction t = session.beginTransaction();
 		
 	
-		SQLQuery query = session.createSQLQuery(" select f.nome as nomeColaborador , enc_ex as encaminhamento, seto.nome as nomeSetor, COUNT(enc_ex) as qtdEncaminhamento from registro_entrada as r inner join funcionario AS f on f.ID_MATRICULA = r.ID_MATRICULA inner join setor AS seto on f.ID_CENTRO_CUSTO = seto.ID_CENTRO_CUSTO  group by f.nome  , enc_ex , seto.nome");
+		SQLQuery query = session.createSQLQuery(" select f.nome as nomeColaborador , enc_ex as encaminhamento, seto.nome as nomeSetor, COUNT(enc_ex) as qtdEncaminhamento from registro_entrada as r inner join funcionario AS f on f.ID_MATRICULA = r.ID_MATRICULA inner join setor AS seto on f.ID_CENTRO_CUSTO = seto.ID_CENTRO_CUSTO  group by enc_ex, f.nome ,  seto.nome");
 	    query.addScalar("nomeColaborador");
 	    query.addScalar("nomeSetor");
 	    query.addScalar("qtdEncaminhamento");
